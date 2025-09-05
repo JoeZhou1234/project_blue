afbb7a553a7992d3adf4395054e0666f582a6032
Xing Yu (Joe) Zhou <mr.joezhou@gmail.com>
2025-09-04 22:10:00 -0400

    Fixed cookie issue

--- backend/app.js
+++ backend/app.js
@@ -29,11 +29,6 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: {
-      secure: process.env.NODE_ENV === 'production',
-      httpOnly: true,
-      maxAge: 24 * 60 * 60 * 1000 // 24 hours
-    }
   }),
 );
 
